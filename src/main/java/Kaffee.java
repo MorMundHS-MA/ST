889a06fd7b41ac1a60c67aca16e53d880d4dfac4
Gurkenkater <coregamer89@hotmail.de>
2017-06-06 18:31:21 +0200

    Die setOptionen() Methode überarbeitet

--- src/main/java/Kaffee.java
+++ src/main/java/Kaffee.java
@@ -1,70 +1,66 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Kaffee extends Produkt {
 
-    public Kaffee() {
-        setBezeichnung("Kaffee");
-        setPreis(120);
-        //setOptionen();
-    }
+	public Kaffee() {
+		setBezeichnung("Kaffee");
+		setPreis(120);
+		// setOptionen();
+	}
 
-    /*
+	/**
+	 * Methode frägt den Kunden via Konsoleausgabe ob er Zusatzoptionen für
+	 * seinen Kaffee wählt, welche er per Konsoleneingabe äußert.
+	 */
+	public void setOptionen() {
 
-    /**
-     * Methode frägt den Kunden via Konsoleausgabe ob er Zusatzoptionen für seinen Kaffee
-     * wählt, welche er per Konsoleneingabe äußert.
-     *
-    public void setOptionen() {
+		String optionen;
 
-        String optionen;
+		// Optionen: Extra Milch, Zucker oder Schockostreusel
+		System.out.println("Eingabe 1: Kaffee wird zubereitet./n"
+				+ "Eingabe 2: Extra Milch./n"
+				+ "Eingabe 3: Extra Zucker./n"
+				+ "Eingabe 4: Extra Schockostreusel."
+				+ "Eingabe 5: Abbruch");
+		inputOptionen();
+	}
 
-        //Optionen: Extra Milch, Zucker oder Schockostreusel
-        System.out.println("Durch einfaches drücken der Eingabetaste wird der Kaffee"
-                + " zubereitet. Sie können aber auch extra Milch, Zucker"
-                + " oder Schockostreusel eingeben und sie bekommen es jeweils"
-                + " für 10ct extra oben drauf. Um eine Option rückgängig zu machen,"
-                + " wählen Sie diese ein zweites Mal an.");
+	private void inputOptionen() {
 
-    private void inputOptionen() {
+		final String[] listeDerOptionen = { "Milch", "Zucker", "Schockostreusel", "Abbruch" };
+		ArrayList<String> gewählteOptionen = new ArrayList();
+		
+		Scanner in = new Scanner(System.in);
+		
+		try {
+			int eingabe = in.nextInt();
+			
+			if(eingabe < 1 || eingabe > 5) {
+				throw new InputMismatchException();
+			}
+			
+			else if(eingabe == 5) {
+				//TODO: Rufe irgendwie KaffeeAutomat.abbruch() auf
+			}
+			
+			else if(eingabe == 1) {
+				/*TODO: Rufe irgendwie KaffeeAutomat.zapfeProdukt() auf,
+				wenn das die Methode sein soll die das Zeug zubereitet.*/
+			}
+			
+			else {
+				gewählteOptionen.add(listeDerOptionen[eingabe]);
+				setOptionen(gewählteOptionen.toArray(new String[gewählteOptionen.size()]));
+			}
+		}
+		
+		catch(InputMismatchException e) {
+			System.out.println("Sehr witzig. Geben Sie bitte eine valide Zahl ein.");
+			inputOptionen();
+		}
 
-        /**
-         * Liste legt fest, welche Optionen gewählt wurden.
-         * Die Erste Option in der Liste ist die Milch,
-         * die Zweite ist Zucker und die Dritte sind
-         * Schockostreusel.
-         *
-        private String[] optionsListe = {null, null, null};
-
-        try {
-            optionen = in.nextLine();
-            if(!optionen.equalsIgnoreCase("Milch" || "Zucker" || "Schockostreusel" || "Abbruch")) {
-                throw new Exception();
-            }
-        }
-        catch(Exception e) {
-            System.out.println("Regen Sie mich bitte nicht auf und konzentrieren Sie"
-                    + " sich bei der Eingabe. Probieren Sie es nochmal.");
-            inputOptionen();
-        }
-
-        if(optionen.equalsIgnoreCase("Abbruch")) {
-            KaffeeAutomatInterface.abbruch();
-        }
-
-        else if(optionen.equals("")) {
-            setOptionen(optionsListe);
-            //TODO: Kehre zur main() zurück und fahre dort fort.
-        }
-
-        else if(optionen.equalsIgnoreCase("Milch")) {
-            (optionsListe[0].equals("Milch") ? String[0] = null : String[0] = "Milch";
-        }
-
-        else if(optionen.equalsIgnoreCase("Zucker")) {
-            (optionsListe[1].equals("Zucker") ? String[1] = null : String[1] = "Zucker";
-        }
-
-        else if(optionen.equalsIgnoreCase("Schockostreusel")) {
-            (optionsListe[2].equals("Schockostreusel") ? String[2] = null : String[2] = "Schockostreusel";
-        }
-    }*/
+	}
 }
-
