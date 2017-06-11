233df8ab780e6ca1c258ddef42f54f5391cb3796
MarPra <marco.96p@hotmail.de>
2017-06-11 16:39:38 +0200

    Implemented methods and attributes

--- Aufgabe2/src/main/java/com/coffee/machine/states/HeisserKakao.java
+++ Aufgabe2/src/main/java/com/coffee/machine/states/HeisserKakao.java
@@ -2,46 +2,59 @@ package com.coffee.machine.states;
 
 import com.coffee.machine.AutomatenStatus;
 import com.coffee.machine.AutomatenSteuerung;
+import com.coffee.machine.Helper;
 
 public class HeisserKakao extends Produkt {
+
+    private final static String[] optionen = new String[]{"Kalt"};
+    private final static int preis = 90;
+    private final static String name = "Heisser Kakao";
+    // Kakao hat keine Optionen
+
     public HeisserKakao(int bereitsBezahlt) {
         super(bereitsBezahlt);
     }
 
     public void bezahleBetrag(AutomatenSteuerung automat, int betrag) {
-
+        automat.changeState(new Bezahlung(this, super.bezahlterBetrag));
     }
 
     public void waehleProdukt(AutomatenSteuerung automat, String produkt) {
-
+        System.out.println("Brechen Sie die momentane Bestellung ab um das Produkt neu zu waehlen.");
     }
 
     public void waehleOption(AutomatenSteuerung automat, String option) {
-
+        if (option.equalsIgnoreCase("Kalt")) {
+            automat.changeState(new KalterKakao(super.bezahlterBetrag));
+        } else {
+            System.out.printf("Optionen %s ist leider nicht verfügbar.", option);
+        }
     }
 
     public int fordereWechselgeld(AutomatenSteuerung automat) {
-        return 0;
+        return bezahlterBetrag;
     }
 
     public int zapfeProdukt(AutomatenSteuerung automat) {
-        return 0;
+        automat.changeState(new Bezahlung(this, super.bezahlterBetrag));
+        return automat.zapfeProdukt();
     }
 
     public int abbruch(AutomatenSteuerung automat) {
-        return 0;
+        automat.changeState(new Leerlauf());
+        return bezahlterBetrag;
     }
 
     protected String[] getMoeglicheOptionen() {
-        return new String[0];
+        return optionen;
     }
 
     public String getProduktName() {
-        return null;
+        return name;
     }
 
     public int getPreis() {
-        return 0;
+        return preis;
     }
 
     public int getOptionsCost() {
