4cb32a17dd7b4b259b1cf42d2352e0a997bbca34
Gurkenkater <coregamer89@hotmail.de>
2017-06-22 09:48:02 +0200

    Push damit alles sauber ist.

--- src/main/java/Kakao.java
+++ src/main/java/Kakao.java
@@ -1,6 +1,8 @@
 public class Kakao extends Produkt{
     public Kakao() {
         //TODO : Was ist nochmal der Preis von Kakao?
-        super("Kakao", 9999, new String[]{"Kalt", "Heiss"});
+        super("Kakao", 90, new String[]{"Kalt", "Heiss"});
     }
+    
+    
 }
